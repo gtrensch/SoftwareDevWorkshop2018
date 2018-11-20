241fa474a22c870bfb4e551b8f753f2dc65120bf
ZoeHes <z.hester@fz-juelich.de>
2018-11-20 14:34:34 +0100

    New Function

---
+++ printNumbers/functions/gausssum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 20 14:12:29 2018
+
+@author: Fienchen
+"""
+
+def SumSequence(pBis=0):
+    retu = 0
+    for n in range(pBis):
+        retu += n+1
+    return retu    
+    
